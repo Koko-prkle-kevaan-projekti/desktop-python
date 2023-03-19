cc4cea64a7dea4b76077a726b73ffc423403ffb0
Mikko Kujala <m.kujala@live.com>
2023-03-19 10:15:06 +0200

    Add info dialogs to gui.

--- tassu_tutka/argparse.py
+++ tassu_tutka/argparse.py
@@ -3,8 +3,7 @@ import argparse
 
 def parse() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    exclusives = parser.add_mutually_exclusive_group()
-    exclusives.required = True
+    exclusives = parser.add_mutually_exclusive_group(required=True)
     exclusives.add_argument("--gui", action="store_true", help="Launch graphical interface.")
     exclusives.add_argument("--ifile", action="store", help="Path to GPS .dat file.")
     parser.add_argument("--ofile", action="store", help="Path to GeoJSON output file. Optional. Defaults to stdout.")
--- tassu_tutka/gui.py
+++ tassu_tutka/gui.py
@@ -1,22 +1,101 @@
 import tkinter
 from tkinter import ttk
 from tkinter.constants import *
-import tkintermapview as tkm
 from tkinter import filedialog
 import io
+import tkintermapview as tkm
 
 
 def user_interface():
     tk = tkinter.Tk()
     tk.title("TassuTutka")
     mw = MainWindow(tk)
+    menu_bar = MenuBar(tk)
+    tk.config(menu=menu_bar)
     tk.mainloop()
+
+
+class InfoDialog(tkinter.Toplevel):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent)
+
+        # self.geometry("400x300")
+        self.title(kwargs.get("title") or "Info Dialog")
+        text = tkinter.Text(
+            self,
+            border=0,
+            padx=10,
+            bg="#f0f0f0",
+            width=90,
+            height=kwargs.get("height", 10),
+        )
+        text.insert(
+            tkinter.END,
+            chars=kwargs.get("info_content", "PLACEHOLDER. SET info_content kwarg"),
+        )
+        text.grid(row=0, column=0, columnspan=3)
+
+        close_btn = ttk.Button(self, text="Sulje", command=lambda: self.destroy())
+        close_btn.grid(row=1, column=1)
+
+
+class MenuBar(tkinter.Menu):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent)
+        file_menu = tkinter.Menu(self, tearoff=0)
+        file_menu.add_command(label="Tyhjennä merkinnät")
+        file_menu.add_command(label="Lopeta", command=lambda: parent.destroy())
+        self.add_cascade(label="Tiedosto", menu=file_menu)
+
+        info_menu = tkinter.Menu(self, tearoff=0)
+        info_menu.add_command(label="Epälisenssi", command=lambda: InfoDialog(
+            parent,
+            title="Epälisenssi",
+            height=25,
+            info_content="""
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute
+this software, either in source code form or as a compiled binary, for any
+purpose, commercial or non-commercial, and by any means.
+
+In jurisdictions that recognize copyright laws, the author or authors of
+this software dedicate any and all copyright interest in the software to the
+public domain. We make this dedication for the benefit of the public at
+large and to the detriment of our heirs and successors. We intend this
+dedication to be an overt act of relinquishment in perpetuity of all present
+and future rights to this software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+            """
+        ))
+        info_menu.add_command(
+            label="Tietoa",
+            command=lambda: InfoDialog(
+                parent,
+                title="Info",
+                info_content="""
+TassuTutka on neljän opiskelijan koira-GPS -projekti. Tämä ohjelma on projektin
+työpöytäympäristöön tarkoitettu ohjelma, joka tulkitsee ja näyttää GPS-laitteelta
+saatavaa dataa. Projektissa mukana: Mikko Kujala, Rebecca Soisenniemi, Nico Hertolin
+sekä Pasi Puhakka.
+            """,
+                height=7,
+            ),
+        )
+        self.add_cascade(label="Tietoa", menu=info_menu)
 
 
 class MainWindow(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent, borderwidth=2, relief=RIDGE)
-        self.grid()
 
         # Text showing selected file.
         self.file_name = tkinter.StringVar(self)
@@ -26,7 +105,7 @@ class MainWindow(ttk.Frame):
 
         # File types for open file dialog.
         ft = (("GPS datatiedostot", "*.gdat"), ("Kaikki tiedostot", "*.*"))
-        self._file: io.TextIOWrapper = None
+        self._file: io.TextIOWrapper | None = None
 
         def read_file() -> None:
             if self.file:
@@ -34,14 +113,13 @@ class MainWindow(ttk.Frame):
             self.file = filedialog.askopenfile(
                 title="Avaa tiedosto", initialdir="~", filetypes=ft
             )
-            self.file_name.set(self.file.name) if self.file else self.file_name.set("Tiedostoa ei ole valittu")
+            self.file_name.set(self.file.name) if self.file else self.file_name.set(
+                "Tiedostoa ei ole valittu"
+            )
 
         # Them buttons
-        read_file = ttk.Button(
-            self,
-            text="Avaa tiedosto...",
-            command=read_file,
-        ).grid(column=0, row=1, sticky="ew")
+        self.read_file = ttk.Button(self, text="Avaa tiedosto...", command=read_file)
+        self.read_file.grid(column=0, row=1, sticky="ew")
         draw_route = ttk.Button(self, text="Piirrä reitti").grid(
             column=1, row=1, sticky="ew"
         )
@@ -55,6 +133,7 @@ class MainWindow(ttk.Frame):
         self.map = tkm.TkinterMapView(self, width=800, height=600, corner_radius=5)
         self.map.set_position(65.0612111, 25.4681883)
         self.map.grid(column=0, row=2, columnspan=4)
+        self.grid()
 
     def set_normal_view(self):
         self.map.set_tile_server("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png")
