469a27f0670d805966de8164c6e3e0068904b1bf
Mikko Kujala <m.kujala@live.com>
2023-03-24 15:47:11 +0200

    taas unohdin tehdä haaran. :(

--- tassu_tutka/api.py
+++ tassu_tutka/api.py
@@ -1,13 +1,41 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel, Field
+
+from tassu_tutka.server import BUF, BUF_LOCK
+
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_item():
-    return "moro"
+@app.get("/get_lines")
+def read_item() -> dict[str, list[str]]:
+    lines = []
+    while line := readline():
+        lines.append(line)
+    return {"lines": lines}
+
+
+def readline() -> str | None:
+    """Return a line from BUF
+
+    Reads a line from the BUF, to which the server facing GPS device writes to.
+    The function will return None if BUF doesn't have a full line.
+    """
+    global BUF
+    line = ""
+    BUF_LOCK.acquire(blocking=True)
+    for i, ch in enumerate(BUF):
+        line += ch
+        if ch == "\n":
+            BUF = BUF[i + 1 :]
+            break
+    else:  # A full line in buffer isn't available.
+        BUF_LOCK.release()
+        return None
+    BUF_LOCK.release()
+    return line
+
 
 def run():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    
+    uvicorn.run(app, host="0.0.0.0", port=8000, workers=0, loop="none")
--- tassu_tutka/server.py
+++ tassu_tutka/server.py
@@ -15,14 +15,16 @@ RX_PORT = 64999
 BUF = ""
 BUF_LOCK = threading.Lock()
 
+
 def _add_pid_to_pidfile(pid: int | None = None):
-    if "windows" in platform.platform().lower() :
+    if "windows" in platform.platform().lower():
         raise error.WindowsError("Wrong os")
     if not pid:
         pid = os.getpid()
     home = os.getenv("HOME")
     with open(f"{home}/ttutka.pid", "a+") as fh:
         fh.write(f"{pid}\n")
+
 
 def _remove_pid_from_pidfile(pid: int | None = None):
     if not pid:
@@ -33,11 +35,13 @@ def _remove_pid_from_pidfile(pid: int | None = None):
     with open(f"{home}/ttutka.pid", "w") as fh:
         fh.writelines(pids)
 
+
 def _get_pids_from_pidfile():
     home = os.getenv("HOME")
     with open(f"{home}/ttutka.pid", "r") as fh:
         pids = [x.strip() for x in fh.readlines()]
     return pids
+
 
 class RxHandler(socketserver.StreamRequestHandler):
     def handle(self) -> None:
@@ -70,6 +74,7 @@ class TxHandler(socketserver.StreamRequestHandler):
 
 def serve():
     import signal
+
     _add_pid_to_pidfile()
 
     logging.info("Starting Rx server in port 65000... Waiting for a GPS device.")
@@ -77,12 +82,13 @@ def serve():
     t_rx = threading.Thread(group=None, target=rx.serve_forever)
     t_rx.start()
 
-    logging.info("Starting Tx server in port 64999...")
-    tx = socketserver.TCPServer(("0.0.0.0", 64999), TxHandler)
-    t_tx = threading.Thread(group=None, target=tx.serve_forever)
-    t_tx.start()
+    # logging.info("Starting Tx server in port 64999...")
+    # tx = socketserver.TCPServer(("0.0.0.0", 64999), TxHandler)
+    # t_tx = threading.Thread(group=None, target=tx.serve_forever)
+    # t_tx.start()
 
     import tassu_tutka.api as tassapi
+
     logging.info("Starting client API.")
     t_api = threading.Thread(group=None, target=tassapi.run)
     t_api.daemon = True
@@ -91,17 +97,16 @@ def serve():
     def quit_(
         a: socketserver.TCPServer,
         a_thread: threading.Thread,
-        b: socketserver.TCPServer,
-        b_thread: threading.Thread,
         sig,
         frame,
     ):
         a.shutdown()
         a.server_close()
-        b.shutdown()
         _remove_pid_from_pidfile()
 
-    quit_ = functools.partial(quit_, rx, t_rx, tx, None)
+    quit_ = functools.partial(quit_, rx, t_rx)
     signal.signal(signal.SIGINT, quit_)
     signal.signal(signal.SIGHUP, quit_)
-    logging.info("Server started.\nPress CTRL+C (SIGINT) or ttutka server command to stop.")
+    logging.info(
+        "Server started.\nPress CTRL+C (SIGINT) or ttutka server command to stop."
+    )
